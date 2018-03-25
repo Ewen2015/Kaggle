45f03fb4b33f882815ee003671b8802881dfe35b
Ewen2015 <eqwang@colostate.edu>
2018-03-25 18:12:18 +0800

    update

--- conversion_rate/Conversion.py
+++ conversion_rate/Conversion.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import time
+import lightgbm as lgb 
+from lightgbm import LGBMClassifier
+from sklearn.metrics import log_loss
+import gossipcat as gc
 
 wd = '/Users/ewenwang/Documents/practice_data/conversion_rate/'
 raw_data = ['round1_ijcai_18_train_20180301.txt', 'round1_ijcai_18_test_a_20180301.txt']
@@ -15,7 +19,7 @@ def save_data(data, file):
 def timestamp_datetime(timestamp):
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
 
-def pre_process(data):
+def Preprocess(data):
 
     for i in range(3):
         data['category_%d'%(i)] = data['item_category_list'].apply(lambda x: x.split(";")[i] if len(x.split(";"))>i else " ")
@@ -61,31 +65,177 @@ features = ['item_id', 'item_brand_id', 'item_city_id',
 'shop_score_service', 'shop_score_delivery', 'shop_score_description', 'shop_review_num_level', 'shop_review_positive_rate', 'shop_star_level', 
 'week', 'hour', 'user_query_day', 'user_query_day_hour']
 
-category_features = ['item_id', 'item_brand_id', 'item_city_id', 
+id_features = ['item_id', 'item_brand_id', 'item_city_id', 
+'user_id', 'user_gender_id', 'user_occupation_id', 
+'context_id', 'context_page_id', 
+'shop_id', ]
+
+numeric_features = ['item_price_level', 'item_sales_level', 'item_collected_level', 'item_pv_level', 
+'user_age_level', 'user_star_level', 
+'shop_score_service', 'shop_score_delivery', 'shop_score_description', 'shop_review_num_level', 'shop_review_positive_rate', 'shop_star_level', 
+'week', 'hour', 'user_query_day', 'user_query_day_hour']
+
+categorical_features = ['item_id', 'item_brand_id', 'item_city_id', 
 'user_id', 'user_gender_id', 'user_occupation_id', 
 'context_id', 'context_page_id', 
 'shop_id', 
 'category_1', 'category_2', 'property_0', 'property_1', 'property_2', 
-'predict_category_0', 'predict_category_1', 'predict_category_2',]
+'predict_category_0', 'predict_category_1', 'predict_category_2']
+
+
+params = {
+    'boosting_type': 'gbdt', 
+    'num_leaves': 31, 
+    'max_depth': -1, 
+    'learning_rate': 0.01,       #
+    'n_estimators': 100, 
+    'subsample_for_bin': 200000, 
+    'objective': None, 
+    'class_weight': None, 
+    'min_split_gain': 0.0, 
+    'min_child_weight': 0.001, 
+    'min_child_samples': 20, 
+    'subsample': 0.75,          #
+    'subsample_freq': 1, 
+    'colsample_bytree': 0.75,   #
+    'reg_alpha': 0.0, 
+    'reg_lambda': 0.0, 
+    'random_state': 2018,       #
+    'n_jobs': -1, 
+    'silent': True
+}
 
 def commom(train, test, category_features):
-    results = pd.DataFrame()
+    results = pd.DataFrame(columns=['column', 'count_test', 'count_train', 'count_common', 'pct_test', 'pct_train'])
     for ind, val in enumerate(category_features):
-        results[ind, 'column'] = val
-        results[ind,'count_test'] = len(test[val].unique())
-        results[ind,'count_train'] = len(train[val].unique())
-        results[ind,'count_common'] = pd.merge(test, train, how='inner', on=[val]).groupby(val).size().shape[0]
-        results[ind,'pct_test'] = results[ind,'count_common']/results[ind,'count_test']
-        results[ind,'pct_train'] = results[ind,'count_common']/results[ind,'count_train']
-    results.sort_values(['pct_test'], accending=False)
-    print(results[ind,:])
+        df = pd.DataFrame()
+        df.loc[ind, 'column']  = val
+        df.loc[ind, 'count_test'] = len(test[val].unique())
+        df.loc[ind, 'count_train']  = len(train[val].unique())
+        df.loc[ind, 'count_common']  = pd.merge(test, train, how='inner', on=[val]).groupby(val).size().shape[0]
+        df.loc[ind, 'pct_test'] = df.loc[ind, 'count_common']/df.loc[ind, 'count_test']
+        df.loc[ind, 'pct_train'] = df.loc[ind, 'count_common']/df.loc[ind, 'count_train']
+        results = results.append(df)
+    results.sort_values(by=['pct_test'], ascending=False)
+    
     return results
 
+class Train(object):
+    """docstring for Train"""
+    def __init__(self, train, test, target, features, validation_day=24):
+        super(Train, self).__init__()
+        self.train = train
+        self.test = test
+        self.target = target
+        self.features = features
+
+        self.dtrain = self.train.loc[self.train.day<validation_day]
+        self.validation = self.train.loc[self.train.day==validation_day]
+
+        self.pred_test = []
+        
+    def Baseline(self):
+        clf = lgb.LGBMClassifier(
+            boosting_type='gbdt', 
+            num_leaves=31, 
+            max_depth=-1, 
+            learning_rate=0.01,         #
+            n_estimators=500,           #
+            subsample_for_bin=200000, 
+            objective=None, 
+            class_weight=None, 
+            min_split_gain=0.0, 
+            min_child_weight=0.001, 
+            min_child_samples=20, 
+            subsample=0.75,             #
+            subsample_freq=1, 
+            colsample_bytree=0.75,      #
+            reg_alpha=0.0, 
+            reg_lambda=0.0, 
+            random_state=2018,          #
+            n_jobs=-1, 
+            silent=True)
+
+        clf.fit(self.dtrain[self.features], self.dtrain[self.target], feature_name=self.features)
+        self.validation['predicted_score'] = clf.predict_proba(self.validation[self.features])[:, 1]
+        print('log loss: ', log_loss(self.validation[self.target], self.validation['predicted_score']))
+
+        return clf
+
+    def simAnneal(self, alpha=0.75, scoring='neg_log_loss', n_jobs=1, results=True, seed=2018):
+        """ Hyper parameter tuning with simulated annealing.
+
+        Employes the simulated annealing to find the optimal hyper parameters and 
+        return an optimized classifier.
+
+        Args:
+            train: A training set of your machine learning project.
+            target: The target variablet; limited to binary.
+            predictors: The predictors.
+            results: Whether print the progress out; default with True.
+
+        Returns:
+            A classifier generated from gbdt with simulated annealing hyper parameter tuning.
+        """
+        params = {
+            'max_depth': [i for i in range(1, 21, 1)],
+            'subsample': [i / 100.0 for i in range(1, 101, 1)],
+            'colsample_bytree': [i / 100.0 for i in range(1, 101, 1)],
+        }
+
+        print('simulating...')
+
+        gbm = LGBMClassifier(
+            boosting_type='gbdt', 
+            num_leaves=31, 
+            max_depth=-1, 
+            learning_rate=0.01,         #
+            n_estimators=1000,           #
+            subsample_for_bin=200000, 
+            objective=None, 
+            class_weight=None, 
+            min_split_gain=0.0, 
+            min_child_weight=0.001, 
+            min_child_samples=20, 
+            subsample=0.75,             #
+            subsample_freq=1, 
+            colsample_bytree=0.75,      #
+            reg_alpha=0.0, 
+            reg_lambda=0.0, 
+            random_state=2018,          #
+            n_jobs=-1, 
+            silent=True
+        )
+        sa = gc.SimulatedAnneal(
+            gbm, 
+            params, 
+            scoring=scoring, 
+            T=10.0, 
+            T_min=0.001, 
+            alpha=alpha,
+            n_trans=5, 
+            max_iter=0.25, 
+            max_runtime=300, 
+            cv=5, 
+            random_state=seed, 
+            verbose=True, 
+            refit=True, 
+            n_jobs=n_jobs)
+        
+        sa.fit(self.dtrain[self.features], self.dtrain[self.target])
+        self.validation['predicted_score'] = sa.best_estimator_.predict_proba(self.validation[self.features])[:, 1]
+
+        if results:
+            print('\nbest score:', '{:.6f}'.format(sa.best_score_),
+                  '\nlog loss:', '{:.6f}'.format(log_loss(self.validation[self.target], self.validation['predicted_score'])),
+                  '\nbest parameters:', str({key: '{:.2f}'.format(value) for key, value in sa.best_params_.items()}))
+
+        return sa
 
 
 if __name__ == '__main__':
-    train, test = load_data(data_list=new_data)
-    results = commom(train, test, category_features=category_features)
+    pass
+
 
 
 
